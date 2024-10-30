df9d54a29a5a5f42fb8a1130daf42563c0db5a62
danipro777 <daniel1603@mesoamericana.edu.gt>
2024-10-30 16:50:43 -0600

    Creando modelo usuarios, modificando archivo gitignore y config.json

---
+++ models/usuarios.js
@@ -0,0 +1,66 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class usuarios extends Model {
+    static associate(models) {
+        // * Aqui van las relaciones
+    }
+  }
+
+  usuarios.init({
+    idUsuario: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    usuario: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    contrasenia: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    token: {
+        type: DataTypes.STRING(255),
+        allowNull: true
+    },
+    tokenExpiresAt: {
+        type: DataTypes.DATETIME,
+        allowNull: true
+    },
+    estado: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    idRol: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+    idSede: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    idPersona: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+    }
+  }, {
+    sequelize,
+    modelName: 'usuarios',
+    tableName: 'usuarios'
+  });
+
+  return usuarios;
+};
